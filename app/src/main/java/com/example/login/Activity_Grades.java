35fec6a2ed534a49a671c0133e1a8a134d706649
tamnt5829 <17521006@gm.uit.edu.vn>
2019-11-22 22:36:26 +0700

    menu

---
+++ app/src/main/java/com/example/login/Activity_Grades.java
@@ -0,0 +1,14 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Grades extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__grades);
+    }
+}
--- app/src/main/java/com/example/login/Activity_Menu.java
+++ app/src/main/java/com/example/login/Activity_Menu.java
@@ -1,65 +1,39 @@
 package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
-import com.androidnetworking.AndroidNetworking;
-import com.androidnetworking.error.ANError;
-import com.androidnetworking.interfaces.StringRequestListener;
-
-import org.json.JSONArray;
+import android.view.View;
 
 public class Activity_Menu extends AppCompatActivity {
 
-    String urlGetData = "http://192.168.1.168:8080/database/getData.php";
-
+    CardView card_checkin;
+    CardView card_grades;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__menu);
 
-        //GetData(urlGetData);21
-        AndroidNetworking.initialize(getApplicationContext());
-        AndroidNetworking.get("http://android-samsung.herokuapp.com/api/category")
-                .build()
-                .getAsString(new StringRequestListener() {
-                    @Override
-                    public void onResponse(String response) {
-                        Toast.makeText(getBaseContext(), response.toString(), Toast.LENGTH_LONG).show();
-                    }
+        //Activity CHECKIN
+        card_checkin = (CardView) findViewById(R.id.cardview_checkin);
+        card_checkin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_cardview_checkin = new Intent(Activity_Menu.this, Activity_Checkin.class);
+                startActivity(intent_cardview_checkin);
+            }
+        });
 
-                    @Override
-                    public void onError(ANError anError) {
-                        Toast.makeText(getBaseContext(), anError.toString(), Toast.LENGTH_LONG).show();
-                    }
-                });
+        //Activity GRADES
+        card_grades = (CardView) findViewById(R.id.cardview_grades);
+        card_grades.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_cardview_grades = new Intent(Activity_Menu.this, Activity_Grades.class);
+                startActivity(intent_cardview_grades);
+            }
+        });
     }
-
-//    private void GetData (String url){
-//        RequestQueue requestQueue = Volley.newRequestQueue(this);
-//        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
-//                new Response.Listener<JSONArray>() {
-//            @Override
-//            public void onResponse(JSONArray response) {
-//                Toast.makeText(Activity_Menu.this,response.toString(), Toast.LENGTH_SHORT).show();
-//            }
-//        }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                Toast.makeText(Activity_Menu.this, "Fail", Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//        );
-//        requestQueue.add(jsonArrayRequest);
-//    }
-
-
 }
